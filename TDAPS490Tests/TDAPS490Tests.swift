2c88059f82dadfecb3250843667bdc627f29f4ee
Sam Wang <samwangyzh@gmail.com>
2021-02-05 05:54:12 -0500

    Fix build issue

--- TDAPS490Tests/TDAPS490Tests.swift
+++ TDAPS490Tests/TDAPS490Tests.swift
@@ -10,20 +10,20 @@ import XCTest
 
 class TDAPS490Tests: XCTestCase {
 
-    override func setUpWithError() throws {
+    override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
@@ -31,3 +31,4 @@ class TDAPS490Tests: XCTestCase {
     }
 
 }
+
