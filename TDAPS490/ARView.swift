d9b6a8fbf292c842435550d39e725c92e5b8970b
Yangfan999 <yangfanli96@gmail.com>
2021-02-19 02:58:40 -0500

    AR

--- TDAPS490/ARView.swift
+++ TDAPS490/ARView.swift
@@ -7,23 +7,45 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ARViewContainer: UIViewRepresentable {
+    typealias UIViewType = ARView
     
-    func makeUIView(context: Context) -> ARView {
+    func makeUIView(context: UIViewRepresentableContext<ARViewContainer>) -> ARView {
         
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+        let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+
+        arView.enableImageRec()
         return arView
-        
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
-    
+}
+
+extension ARView: ARSessionDelegate {
+    func enableImageRec() {
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+          fatalError("Missing expected asset catalog resources.")
+        }
+              
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+              
+        self.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+
+        self.session.delegate = self
+      }
+
+    public func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        if !anchors.isEmpty {
+            for anchor in anchors {
+                if let imageAnchor = anchor as? ARImageAnchor {
+                    let imageName = imageAnchor.referenceImage.name
+                    // 能看到你加进去的图片的名字print出来
+                    print(imageName)
+                }
+            }
+        }
+    }
 }
