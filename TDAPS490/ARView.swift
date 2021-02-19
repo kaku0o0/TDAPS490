0ce66d5f187793f347220020bc425c9f0a79dec0
Yangfan999 <yangfanli96@gmail.com>
2021-02-18 22:43:39 -0500

    Structure

---
+++ TDAPS490/ARView.swift
@@ -0,0 +1,29 @@
+//
+//  ARView.swift
+//  TDAPS490
+//
+//  Created by Yangfan Li on 2021-02-18.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+         
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
--- TDAPS490/ContentView.swift
+++ TDAPS490/ContentView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import RealityKit
 
 struct ContentView : View {
     var body: some View {
@@ -29,26 +28,6 @@ struct ContentView : View {
             }
         }
     }
-}
-
-struct ARViewContainer: UIViewRepresentable {
-    
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
-        return arView
-        
-    }
-    
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
 }
 
 #if DEBUG
