9c0ee5cfa00c4a12d86502aeeff6978be9966727
David Liang <77254473+ecetrainer-jojo@users.noreply.github.com>
2021-03-10 22:14:27 -0500

    Add files via upload

---
+++ CardScanner function/Card_scanner_main/CardScanner/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CardScanner
+//
+//  Created by Narlei Moreira on 09/30/2020.
+//  Copyright (c) 2020 Narlei Moreira. All rights reserved.
+//
+
+import UIKit
+import CardScanner
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet var resultsLabel: UILabel!
+
+    @IBAction func scanPaymentCard(_ sender: Any) {
+        
+        // Add NSCameraUsageDescription to your Info.plist
+        let scannerView = CardScanner.getScanner { card, date, cvv in
+            self.resultsLabel.text = "\(card))"
+        }
+        present(scannerView, animated: true, completion: nil)
+    }
+}
